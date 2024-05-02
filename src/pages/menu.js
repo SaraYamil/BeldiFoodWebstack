d13fa7b82396290085234968f00212f791162eee
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2024-05-02 16:39:05 +0100

    Update variable class with className

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,75 +2,75 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>    <section class="menu-header">
+    <div>    <section className="menu-header">
     <div>
         <h1>Menu</h1>
         <h2>Experience Morocco on Your Plate.</h2>
     </div>
 </section>
 
-<section class="breakfastmenu">
-    <h2 class="title">Breakfast Menu</h2>
-    <div class="content">
+<section className="breakfastmenu">
+    <h2 className="title">Breakfast Menu</h2>
+    <div className="content">
         <div>
-            <div class="breakfastmenu-image">
+            <div className="breakfastmenu-image">
                 <img src="assest/images/breakfast/bk1.jpg"/>
             </div>
-            <div class="breakfastmenu-info">
-                <h2 class="breakfastmenu-title">
+            <div className="breakfastmenu-info">
+                <h2 className="breakfastmenu-title">
                     <span>The Trio</span>
-                    <span class="price-tag">78&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">78&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4.4/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="breakfastmenu-image">
+            <div className="breakfastmenu-image">
                 <img src="assest/images/breakfast/bk2.jpg"/>
             </div>
-            <div class="breakfastmenu-info">
-                <h2 class="breakfastmenu-title">
+            <div className="breakfastmenu-info">
+                <h2 className="breakfastmenu-title">
                     <span>Sfenj</span>
-                    <span class="price-tag">36&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">36&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4.5/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="breakfastmenu-image">
+            <div className="breakfastmenu-image">
                 <img src="assest/images/breakfast/bk3.jpg"/>
             </div>
-            <div class="breakfastmenu-info">
-                <h2 class="breakfastmenu-title">
+            <div className="breakfastmenu-info">
+                <h2 className="breakfastmenu-title">
                     <span>Khlie</span>
-                    <span class="price-tag">40&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">40&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>3/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
             </div>
@@ -78,47 +78,47 @@ export default function Menu() {
     </div>
 </section>
 
-<section class="lunchmenu">
-    <h2 class="title">Lunch Menu</h2>
-    <div class="content">
+<section className="lunchmenu">
+    <h2 className="title">Lunch Menu</h2>
+    <div className="content">
         <div>
-            <div class="lunchmenu-image">
+            <div className="lunchmenu-image">
                 <img src="assest/images/lunch/ln1.jpg"/>
             </div>
-            <div class="lunchmenu-info">
-                <h2 class="lunchmenu-title">
+            <div className="lunchmenu-info">
+                <h2 className="lunchmenu-title">
                     <span>Seafood Pastilla</span>
-                    <span class="price-tag">250&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">250&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="lunchmenu-image">
+            <div className="lunchmenu-image">
                 <img src="assest/images/lunch/ln2.jpg"/>
             </div>
-            <div class="lunchmenu-info">
-                <h2 class="lunchmenu-title">
+            <div className="lunchmenu-info">
+                <h2 className="lunchmenu-title">
                     <span>Beef Tagine with Prunes</span>
-                    <span class="price-tag">190&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">190&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4.2/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
             </div>
@@ -126,68 +126,68 @@ export default function Menu() {
     </div>
 </section>
 
-<section class="dinnermenu">
-    <h2 class="title">Dinner Menu</h2>
-    <div class="content">
+<section className="dinnermenu">
+    <h2 className="title">Dinner Menu</h2>
+    <div className="content">
         <div>
-            <div class="dinnermenu-image">
+            <div className="dinnermenu-image">
                 <img src="assest/images/dinner/dn1.jpg"/>
             </div>
-            <div class="dinnermenu-info">
-                <h2 class="dinnermenu-title">
+            <div className="dinnermenu-info">
+                <h2 className="dinnermenu-title">
                     <span>Seffa</span>
-                    <span class="price-tag">120&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">120&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4.4/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="dinnermenu-image">
+            <div className="dinnermenu-image">
                 <img src="assest/images/dinner/dn2.jpg"/>
             </div>
-            <div class="dinnermenu-info">
-                <h2 class="dinnermenu-title">
+            <div className="dinnermenu-info">
+                <h2 className="dinnermenu-title">
                     <span>Kebab</span>
-                    <span class="price-tag">150&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">150&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>5/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="dinnermenu-image">
+            <div className="dinnermenu-image">
                 <img src="assest/images/dinner/dn3.jpg"/>
             </div>
-            <div class="dinnermenu-info">
-                <h2 class="dinnermenu-title">
+            <div className="dinnermenu-info">
+                <h2 className="dinnermenu-title">
                     <span>Harira</span>
-                    <span class="price-tag">50&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">50&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>3/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
             </div>
@@ -195,68 +195,68 @@ export default function Menu() {
     </div>
 </section>
 
-<section class="sweetmenu">
-    <h2 class="title">Sweet Menu</h2>
-    <div class="content">
+<section className="sweetmenu">
+    <h2 className="title">Sweet Menu</h2>
+    <div className="content">
         <div>
-            <div class="sweetmenu-image">
+            <div className="sweetmenu-image">
                 <img src="assest/images/sweet/swt1.jpg"/>
             </div>
-            <div class="sweetmenu-info">
-                <h2 class="sweetmenu-title">
+            <div className="sweetmenu-info">
+                <h2 className="sweetmenu-title">
                     <span>Mhancha</span>
-                    <span class="price-tag">180&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">180&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4.4/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="sweetmenu-image">
+            <div className="sweetmenu-image">
                 <img src="assest/images/sweet/swt2.jpg"/>
             </div>
-            <div class="sweetmenu-info">
-                <h2 class="sweetmenu-title">
+            <div className="sweetmenu-info">
+                <h2 className="sweetmenu-title">
                     <span>Beldi Sweets  </span>
-                    <span class="price-tag">130&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">130&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>3.5/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star-half-stroke"></i>
-                        <i class="fa-solid fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star-half-stroke"></i>
+                        <i className="fa-solid fa-star"></i>
                     </p>
                 </div>
             </div>
         </div>
         <div>
-            <div class="sweetmenu-image">
+            <div className="sweetmenu-image">
                 <img src="assest/images/sweet/swt3.jpg"/>
             </div>
-            <div class="sweetmenu-info">
-                <h2 class="sweetmenu-title">
+            <div className="sweetmenu-info">
+                <h2 className="sweetmenu-title">
                     <span>Jawhara</span>
-                    <span class="price-tag">70&nbsp;<small>MAD</small></span>
+                    <span className="price-tag">70&nbsp;<small>MAD</small></span>
                 </h2>
-                <div class="reviews-container">
+                <div className="reviews-container">
                     <h3>4/5</h3>
-                    <p href="#" class="reviews">
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-solid fa-star"></i>
-                        <i class="fa-regular fa-star"></i>
+                    <p href="#" className="reviews">
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-solid fa-star"></i>
+                        <i className="fa-regular fa-star"></i>
                     </p>
                 </div>
             </div>
