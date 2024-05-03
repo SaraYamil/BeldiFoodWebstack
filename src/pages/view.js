f8749d83ea0d06edcee4bfeea2a25ac3cc364e89
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2024-05-03 22:42:01 +0100

    Update view

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,59 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function View() {
   return (
-    <div><div id="container">	
-
+    <div>
+      <div id="container">
         <div className="product-details">
-            
-        
-        <h1>Biru Putaran</h1>
+          <h1>Sfenj Donuts </h1>
 
-    
-        <span className="hint-star star">
+          <span className="hint-star star">
             <i className="fa fa-star" aria-hidden="true"></i>
             <i className="fa fa-star" aria-hidden="true"></i>
             <i className="fa fa-star" aria-hidden="true"></i>
             <i className="fa fa-star-half-o" aria-hidden="true"></i>
             <i className="fa fa-star-o" aria-hidden="true"></i>
-        </span>
-            
-        
+          </span>
 
-            <p className="information">"  Moroccan delicacy, are deep-fried treats known for their fluffy texture and irresistible sweetness. Made simple yeast dough, these doughnuts are typically coated in sugar or dipped in honey, making them a delightful indulgence enjoyed across Morocco.. "</p>
-    
-            
-            
+          <p className="information">
+            " Moroccan delicacy, are deep-fried treats known for their fluffy
+            texture and irresistible sweetness. Made simple yeast dough, these
+            doughnuts are typically coated in sugar or dipped in honey, making
+            them a delightful indulgence enjoyed across Morocco.. "
+          </p>
 
-    <div className="control">
-        
+          <div className="control">
+            <button
+              type="button"
+              className="custom-btn btn btn-danger"
+              data-bs-toggle="modal"
+              data-bs-target="#BookingModal"
+            >
+              Reservation
+            </button>
+          </div>
+        </div>
 
-        <button className="btnview">
-   
-         <span className="price">30 dh</span>
-   
-       <span className="shopping-cart"><i className="fa fa-shopping-cart" aria-hidden="true"></i></span>
-
-       <span className="buy">Book now</span>
-     </button>
-
+        <div className="product-image">
+          <img src="assets/images/breakfast/bk2.jpg" alt="" />
+        </div>
+      </div>
     </div>
-                
-    </div>
-        
-        
-    <div className="product-image">
-        
-        <img src="assets/images/breakfast/bk2.jpg" alt=""/>
-
-  
-    </div>
-
-    
-    
-    </div>
-    
-    
-    </div>
-  )
+  );
 }
