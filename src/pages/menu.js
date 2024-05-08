866ae2070535d1f692205eee1a899a2997c43d96
SaraYamil <43785364+SaraYamil@users.noreply.github.com>
2024-05-08 15:18:31 +0100

    Update menu with fetch data from api

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,316 +1,79 @@
-import React, { Fragment } from "react";
+import React, { useState, Fragment, useContext, createContext } from "react";
+import { Link } from "react-router-dom";
+import FoodContext from "../FoodContext.jsx";
 
-function Menu(){
-    return(
-        <Fragment>
-             <main>
-
-<header className="baldi-header baldi-menu-header">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-lg-10 col-12 mx-auto">
+function Menu() {
+  const { foods, setFoods } = useContext(FoodContext);
+  return (
+    <Fragment>
+      <main>
+        <header className="baldi-header baldi-menu-header">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-10 col-12 mx-auto">
                 <h1 className="text-white">Menu</h1>
 
-                <strong className="text-white">Experience Morocco on Your Plate</strong>
+                <strong className="text-white">
+                  Experience Morocco on Your Plate
+                </strong>
+              </div>
             </div>
+          </div>
 
-        </div>
-    </div>
+          <div className="overlay"></div>
+        </header>
 
-    <div className="overlay"></div>
-</header>
-
-<section className="menu section-padding">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-12">
-                <h2 className="mb-lg-5 mb-4">Breakfast Menu</h2>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/breakfast/bk1.jpg" className="img-fluid menu-image" alt=""/>
+        <section className="menu section-padding">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h2 className="mb-lg-5 mb-4"> Menu</h2>
+              </div>
+              {foods?.map((food) => (
+                <div key={food.id} className="col-lg-4 col-md-6 col-12">
+                  <div className="menu-thumb">
+                    <img
+                      src={food.image}
+                      className="img-fluid menu-image"
+                      alt=""
+                    />
 
                     <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Moroccan Trio</h4>
+                      <h4 className="mb-0">{food.name}</h4>
 
-                        <span className="price-tag bg-white shadow-lg ms-4">78&nbsp;<small>MAD</small></span>
+                      <span className="price-tag bg-white shadow-lg ms-4">
+                        {food.price}
+                      </span>
 
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4.4/5</h6>
+                      <div className="d-flex flex-wrap align-items-center w-100 mt-2">
+                        <h6 className="reviews-text mb-0 me-3">
+                          {food.review}
+                        </h6>
 
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-          <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
+                        <i>
+                          <img src="assets/images/stars.png" alt="" />
+                        </i>
+                        <Link to={`/view/${food.id}`}>
+                          <button
+                            type="button"
+                            className="custom-btn btn btn-danger"
+                          >
+                            View more
+                          </button>
+                        </Link>
+                      </div>
                     </div>
+                  </div>
                 </div>
+              ))}
+
+              {/*end*/}
             </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/breakfast/bk2.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Sfenj Donuts</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">36&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4.1/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/breakfast/bk3.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Khlii &amp; Eggs</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">40&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">3/5</h6>
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<section className="menu section-padding bg-white">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-12">
-                <h2 className="mb-lg-5 mb-4">Lunch Menu</h2>
-            </div>
-
-            <div className="col-lg-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/lunch/ln1.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Seafood Pastilla</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">250&nbsp;<small>MAD</small></span>
-
-                        <del className="ms-4">400 MAD</del>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4.2/5</h6>
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/lunch/ln2.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Beef Tagine with Prunes</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">190&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-<section className="menu section-padding">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-12">
-                <h2 className="mb-lg-5 mb-4">Dinner Menu</h2>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/dinner/dn1.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Seffa</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">120&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4.4/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/dinner/dn2.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Kebab</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">150&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">3/5</h6>
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/dinner/dn3.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Harira</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">50&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">3/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
- <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-
-<section className="menu section-padding">
-    <div className="container">
-        <div className="row">
-
-            <div className="col-12">
-                <h2 className="mb-lg-5 mb-4">Sweet Menu</h2>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/sweet/swt1.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Mhancha</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">180&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">4.4/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/sweet/swt2.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Beldi Sweets</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">130&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">3/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-12">
-                <div className="menu-thumb">
-                    <img src="assets/images/sweet/swt3.jpg" className="img-fluid menu-image" alt=""/>
-
-                    <div className="menu-info d-flex flex-wrap align-items-center">
-                        <h4 className="mb-0">Jawhara</h4>
-
-                        <span className="price-tag bg-white shadow-lg ms-4">70&nbsp;<small>MAD</small></span>
-
-                        <div className="d-flex flex-wrap align-items-center w-100 mt-2">
-                            <h6 className="reviews-text mb-0 me-3">3/5</h6>
-
-                            <i><img src="assets/images/stars.png" alt=""/></i>
-         <a href="dtl" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="custom-btn btn btn-danger">View Details</button>
-      </a>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-</section>
-
-</main>
-        </Fragment>
-    )
+          </div>
+        </section>
+      </main>
+    </Fragment>
+  );
 }
 
 export default Menu;
