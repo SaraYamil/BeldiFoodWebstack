778c1b5706555bd79a2c8ddd7badc81d8a0a0b94
SARA YAMIL <sarayamil@gmail.com>
2024-05-09 16:03:48 +0100

    Updates

---
+++ static/js/952.bfad7692.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkme=self.webpackChunkme||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.bfad7692.chunk.js.map
